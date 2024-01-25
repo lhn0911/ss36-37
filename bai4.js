159c22a179c154d1d6acc8a8fd4f4b4880b59d5a
lhn0911 <154567669+lhn0911@users.noreply.github.com>
2024-01-25 10:19:10 +0700

    Update bai4.js

--- bai4.js
+++ bai4.js
@@ -1,26 +1,13 @@
-let input1 = document.getElementById("input1");
-let input2 = document.getElementById("input2");
-let resultInput = document.getElementById("resultInput");
-let resultDiv = document.getElementById("resultDiv");
-
-// Cách 1 - Sử dụng input và sự kiện input
-input1.addEventListener('input', calculateSum);
-input2.addEventListener('input', calculateSum);
-
-// Cách 2 - Sử dụng div và sự kiện input
-input1.addEventListener('input', calculateSumDiv);
-input2.addEventListener('input', calculateSumDiv);
-
-function calculateSum() {
-    let value1 = parseFloat(input1.value) || 0;
-    let value2 = parseFloat(input2.value) || 0;
-    let sum = value1 + value2;
-    resultInput.value = sum;
+let a = document.getElementById("a");
+let b = document.getElementById("b");
+let manualSumInput = document.getElementById("manualSum");
+let autoSumInput = document.getElementById("autoSumInput");
+let btn = document.getElementById("btn");
+btn.onclick = function () {
+    manualSumInput.value = Number(a.value) + Number(b.value);
 }
-
-function calculateSumDiv() {
-    let value1 = parseFloat(input1.value) || 0;
-    let value2 = parseFloat(input2.value) || 0;
-    let sum = value1 + value2;
-    resultDiv.value = sum;
+function autoUpdateSum() {
+    autoSumInput.value = Number(a.value) + Number(b.value);
 }
+a.addEventListener('input', autoUpdateSum);
+b.addEventListener('input', autoUpdateSum);
